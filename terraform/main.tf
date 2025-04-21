716f5e5819c9c31060f73afa5e13e11dee63e52d
Matthew Flesher <atlanticcitytc2@gmail.com>
2025-04-21 17:59:26 -0500

    changed username

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,7 +41,7 @@ resource "aws_security_group" "selenium_sg" {
 }
 
 resource "aws_instance" "selenium_hub" {
-  ami           = "ami-0c02fb55956c7d316"  # Ubuntu 20.04 (us-east-1)
+  ami           = "ami-0c02fb55956c7d316"
   instance_type = "t3.micro"
   key_name      = aws_key_pair.deployer.key_name
   vpc_security_group_ids = [aws_security_group.selenium_sg.id]
