c538b856d154f4236f6f8ece1d5750096abb9b4d
Matthew <normy1979@gmail.com>
2025-05-24 16:56:22 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,6 +34,13 @@ resource "aws_security_group" "selenium_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     description = "Allow all outbound traffic"
     from_port   = 0
